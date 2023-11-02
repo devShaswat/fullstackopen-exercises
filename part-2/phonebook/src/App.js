a46e384ef0fb6652224d5af273a5c5323b1a6485
Shaswat Pandey <shaswat.contact@gmail.com>
2023-11-02 15:03:11 +0530

    updating phonebook with Exercise 2.6

--- part-2/phonebook/src/App.js
+++ part-2/phonebook/src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+//  **************   Exercise - 2.6   ************   The Phonebook step 1
+
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const addNewName = (event) => {
+    event.preventDefault()       // preventing default actions, such as page refresh & re-rendering
+    // console.log('hey! i m the event occured ', event.target.value)
+    const newPerson = {name: newName}
+    setPersons(persons.concat(newPerson))
+    console.log(persons)
+    setNewName('')
+  }
+
+  const handleNewName = (event) => {
+    console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addNewName}>
+        <div>
+          name: <input value={newName}
+                       onChange={handleNewName}
+                       placeholder='type here...'
+                />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => <div key={person.name}> {person.name} </div>)}
+      
+      {/* <div>debug: {newName}</div> */}
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
+
+
+
+
